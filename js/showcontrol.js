b432b2411cbb7d3192d35953bd3aafea05a0e245
Luis Quesada <elezeta@gmail.com>
2025-05-10 09:46:47 +0200

    Super simplify widget hide logic, turn into disable, this fixes many quirks with the new frontend

--- js/showcontrol.js
+++ js/showcontrol.js
@@ -105,39 +105,15 @@ function inpaintCropAndStitchHandler(node) {
     return;
 }
 
-let origProps = {};
-
 const findWidgetByName = (node, name) => {
     return node.widgets ? node.widgets.find((w) => w.name === name) : null;
 };
 
-const doesInputWithNameExist = (node, name) => {
-    // See https://github.com/jags111/efficiency-nodes-comfyui/pull/315
-    return false;
-};
-
-const HIDDEN_TAG = "tschide";
 // Toggle Widget + change size
 function toggleWidget(node, widget, show = false, suffix = "") {
-    if (!widget || doesInputWithNameExist(node, widget.name)) return;
-            
-    // Store the original properties of the widget if not already stored
-    if (!origProps[widget.name]) {
-        origProps[widget.name] = { origType: widget.type, origComputeSize: widget.computeSize };
-    }       
-        
-    const origSize = node.size;
-
-    // Set the widget type and computeSize based on the show flag
-    widget.type = show ? origProps[widget.name].origType : HIDDEN_TAG + suffix;
-    widget.computeSize = show ? origProps[widget.name].origComputeSize : () => [0, -4];
-    
-    // Recursively handle linked widgets if they exist
+    if (!widget) return;
+    widget.disabled = !show
     widget.linkedWidgets?.forEach(w => toggleWidget(node, w, ":" + widget.name, show));
-        
-    // Calculate the new height for the node based on its computeSize method
-    const newHeight = node.computeSize()[1];
-    node.setSize([node.size[0], newHeight]);
 }   
 
 app.registerExtension({
