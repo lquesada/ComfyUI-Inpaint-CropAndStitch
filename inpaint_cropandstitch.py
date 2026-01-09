daa7e85a69248f09dbc34269839a81bcc8c13589
Luis Quesada <elezeta@gmail.com>
2026-01-09 19:33:53 +0100

    Set default to gpu

--- inpaint_cropandstitch.py
+++ inpaint_cropandstitch.py
@@ -1125,7 +1125,7 @@ class InpaintCropImproved:
                 "output_padding": (["0", "8", "16", "32", "64", "128", "256", "512"], {"default": "32"}),
                 
                 # Device Mode
-                "device_mode": (["cpu (compatible)", "gpu (much faster)"], {"default": "cpu (compatible)"}),
+                "device_mode": (["cpu (compatible)", "gpu (much faster)"], {"default": "gpu (much faster)"}),
            },
            "optional": {
                 # Optional inputs
